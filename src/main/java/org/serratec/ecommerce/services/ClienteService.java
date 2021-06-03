f84ad16969836ff3b6e043628bd401f4298ab23b
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-06-03 16:05:58 -0300

    CRUD do cliente

---
+++ src/main/java/org/serratec/ecommerce/services/ClienteService.java
@@ -0,0 +1,69 @@
+package org.serratec.ecommerce.services;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.serratec.ecommerce.entities.ClienteEntity;
+import org.serratec.ecommerce.exceptions.ClienteNotFoundException;
+import org.serratec.ecommerce.repositorys.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	ClienteRepository repository;
+	
+	public List<ClienteEntity> getAll() {
+		return repository.findAll();
+	}
+	
+	public ClienteEntity findById(Long id) throws ClienteNotFoundException {
+		Optional<ClienteEntity> cliente = repository.findById(id);
+		if (cliente.isPresent()) {
+			return cliente.get();
+		}
+		throw new ClienteNotFoundException("Cliente não encontrado!");
+	}
+	
+	public ClienteEntity create(ClienteEntity novoCliente) {
+		novoCliente.setAtivo(true);
+		return repository.save(novoCliente);
+	}
+	
+	public ClienteEntity update(ClienteEntity novoCliente) throws ClienteNotFoundException {
+		ClienteEntity cliente = this.findById(novoCliente.getId());
+		if (novoCliente.getEmail() != null) {
+			cliente.setEmail(novoCliente.getEmail());
+		}
+		if (novoCliente.getUsername() != null) {
+			cliente.setUsername(novoCliente.getUsername());
+		}
+		if (novoCliente.getSenha() != null) {
+			cliente.setSenha(novoCliente.getSenha());
+		}
+		if (novoCliente.getNome() != null) {
+			cliente.setNome(novoCliente.getNome());
+		}
+		if (novoCliente.getCpf() != null) {
+			cliente.setCpf(novoCliente.getCpf());
+		}
+		if (novoCliente.getTelefone() != null) {
+			cliente.setTelefone(novoCliente.getTelefone());
+		}
+		if (novoCliente.getData_de_nascimento() != null) {
+			cliente.setData_de_nascimento(novoCliente.getData_de_nascimento());
+		}
+		return repository.save(cliente);
+	}
+	
+	public String delete(Long id) throws ClienteNotFoundException {
+		ClienteEntity cliente = this.findById(id);
+		cliente.setAtivo(false);
+		System.out.println(cliente.isAtivo());
+		repository.save(cliente);
+		return "Cliente deletado com sucesso!";
+	}
+}
