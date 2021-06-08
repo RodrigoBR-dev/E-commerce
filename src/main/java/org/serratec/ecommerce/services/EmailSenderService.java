492e62f2c88850fe4d90f3fc49f34dd02006bde9
JesseTrainee <a.l.i.t.t.l.e@hotmail.com>
2021-06-08 11:35:09 -0300

    email implementado

---
+++ src/main/java/org/serratec/ecommerce/services/EmailSenderService.java
@@ -0,0 +1,22 @@
+package org.serratec.ecommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Configuration
+public class EmailSenderService {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public void sendSimpleMessage(String destinatario, String assunto, String texto) {
+		SimpleMailMessage mensagem = new SimpleMailMessage();
+		mensagem.setFrom("dev.jc2021@gmail.com");
+		mensagem.setTo(destinatario);
+		mensagem.setSubject(assunto);
+		mensagem.setText(texto);
+		mailSender.send(mensagem);
+	}
+}
