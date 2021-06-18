a5584ba44aa73da5e32647c72d6ab18f5e590ef0
RodrigoBR-dev <rodrigocmartins@hotmail.com>
2021-06-17 21:02:34 -0300

    @CrossOrigin(*) em todos os controllers

--- src/main/java/org/serratec/ecommerce/controllers/EnderecoController.java
+++ src/main/java/org/serratec/ecommerce/controllers/EnderecoController.java
@@ -12,6 +12,7 @@ import org.serratec.ecommerce.services.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/endereco")
 public class EnderecoController {
